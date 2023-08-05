3ef6a297dbddea597110272d963f0d4c995146ab
Artem Kopytko <artem.kop19@gmail.com>
2023-08-05 14:12:38 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _260._Single_Number_III
+{
+    internal class Program
+    {
+        public static int[] SingleNumber(int[] nums)
+        {
+            var dict = new Dictionary<int, int>(nums.Length / 2 + 1);
+            foreach (var num in nums)
+            {
+                if (!dict.ContainsKey(num)) dict.Add(num, 1);
+                else
+                {
+                    dict[num]++;
+                }
+            }
+            return dict.Where(p => p.Value == 1).Select(p => p.Key).ToArray();
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
